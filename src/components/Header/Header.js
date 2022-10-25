08da7c9064cc345b1f0113be22324dafbc1fb788
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-25 08:22:48 +0600

    bg added in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,37 +3,36 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import "./Header.css"
 
 const Header = () => {
   return (
-    <Navbar bg="light" fixed="top" expand="lg">
+    <Navbar className="navbar w-100" fixed="top" expand="lg">
       <Container>
-        <Navbar.Brand href="/home" className="fw-bold">
-          Courseella
-        </Navbar.Brand>
+          <Link to="/" className="fw-bold text-decoration-none fs-3 text-white">Courseella</Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Link
-              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              className="text-decoration-none text-white my-1 my-sm-0 mx-3 "
               to="/home"
             >
               Home
             </Link>
             <Link
-              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              className="text-decoration-none text-white my-1 my-sm-0 mx-3 "
               to="/courses"
             >
               Courses
             </Link>
             <Link
-              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              className="text-decoration-none text-white my-1 my-sm-0 mx-3 "
               to="/faq"
             >
               FAQ
             </Link>
             <Link
-              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              className="text-decoration-none text-white my-1 my-sm-0 mx-3 "
               to="/blog"
             >
               Blog
