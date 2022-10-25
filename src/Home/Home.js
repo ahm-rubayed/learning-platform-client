88754eca6b0758f49c79974ff838deed41b006b6
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-25 07:54:10 +0600

    home page complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -9,19 +9,25 @@ const Home = () => {
     <div className="home">
       <div className="overlay">
         <div className="home-item text-white">
-            <small className="text-uppercase fw-bold fst-italic">Welcome to courseella</small>
+          <small className="text-uppercase fw-bold fst-italic">
+            Welcome to courseella
+          </small>
           <h1 className="fw-bold mt-2">Learn without limits...</h1>
           <p className="desc-color">
             Start, switch, or advance your career with more than 5,200 courses,
             Professional Certificates, and degrees from world-class universities
             and companies.
           </p>
-          <Button className="btn-color border-0 mt-3">
-            <Link to="/courses" className="text-white text-decoration-none">Get started now <FaArrowRight/> </Link>
-          </Button>
-          <Button className="ms-2 mt-3 btn-color border-0">
-            <Link to="/courses" className="text-white text-decoration-none">View Course <FaArrowRight/></Link>
-          </Button>
+            <Button className="btn-color border-0 mt-2 d-block d-sm-inline">
+              <Link to="/courses" className="text-white text-decoration-none">
+                Get started now <FaArrowRight />{" "}
+              </Link>
+            </Button>
+            <Button className="mt-2 btn-color border-0 ms-0 ms-sm-2">
+              <Link to="/courses" className="text-white text-decoration-none">
+                View Course <FaArrowRight />
+              </Link>
+            </Button>
         </div>
       </div>
     </div>
