47c7a5e71564d5dc50a28ba87c1f1e807ce67979
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-25 11:03:52 +0600

    Firebase authenticated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth.context';
 import "./Login.css"
 
 const Login = () => {
+    const {login} = useContext(AuthContext)
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const pass = form.password.value
+
+        console.log(email, pass)
+    }
+
+
     return (
         <div className='login'>
             <div className="bg-white shadow text-center login-item rounded mt-3 mx-auto position-absolute top-50 start-50 translate-middle">
                 <h1 className='text-center'>Login</h1>
-                <form className='mt-5'>
-                <input type="email" placeholder='Email' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
-                <input type="password" placeholder='Password' className='form-control d-inline-block w-100 border-0 border-bottom text-muted' />
-               <Button className='btn-submit mt-4 w-100'>
-                <Link className='text-decoration-none text-white text-uppercasen'>Login</Link>
-               </Button>
+                <form onSubmit={handleSubmit} className='mt-5'>
+                <input name="email" type="email" placeholder='Email' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
+                <input name="password" type="password" placeholder='Password' className='form-control d-inline-block w-100 border-0 border-bottom text-muted' />
+                <input type="submit" value="Login" className='bg-none btn-submit mt-4 w-100 border-0 text-white py-2 rounded' />
                 </form>
                 <p className='mt-5'>Don't have an account? 
                     <Link to="/register" className='fw-bold'> Sign Up</Link>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth.context';
 // import "./Login.css"
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext)
+
+    // const [userInfo, setUserInfo] = useState({
+    //     email: "",
+    //     password: ""
+    // });
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password)
+
+        // createUser(userInfo.email, userInfo.password)
+        // .then(result => {
+        //     const user = result.user;
+        //     console.log(user)
+        // })
+        // .catch(error => console.error(error))
+
+        createUser(email, password)
+        .then(r => {
+            const user = r.user;
+            console.log(user)
+        })
+        .catch(e => console.error(e))
+    }
+
     return (
         <div className='login'>
         <div className="bg-white shadow text-center login-item rounded mt-3 mx-auto position-absolute top-50 start-50 translate-middle">
             <h1 className='text-center'>Register</h1>
-            <form className='mt-5'>
-            <input type="text" placeholder='Name' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
-            <input type="email" placeholder='Email' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
-            <input type="password" placeholder='Password' className='form-control d-inline-block w-100 border-0 border-bottom text-muted' />
-           <Button className='btn-submit mt-4 w-100'>
-            <Link className='text-decoration-none text-white text-uppercasen'>Sign Up</Link>
-           </Button>
+            <form onSubmit={handleSubmit} className='mt-5'>
+            <input name="name" type="text" placeholder='Name' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
+            <input name="email" type="email" placeholder='Email' className='form-control d-inline-block w-100 my-3 border-0 border-bottom text-muted' />
+            <input name="password" type="password" placeholder='Password' className='form-control d-inline-block w-100 border-0 border-bottom text-muted' />
+            <input type="submit" value="Sign Up" className='bg-none btn-submit mt-4 w-100 border-0 text-white py-2 rounded' />
+
             </form>
             <p className='mt-5'>Already have an account?
                 <Link to="/login" className='fw-bold'> Login</Link>
