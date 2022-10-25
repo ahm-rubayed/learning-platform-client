7c1c370808927c2a68a81dd636767c0e2ab0cc71
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-25 06:53:22 +0600

    Home section complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./Home.css";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
-    return (
-        <div>
-            Home
+  return (
+    <div className="home">
+      <div className="overlay">
+        <div className="home-item text-white">
+            <small className="text-uppercase fw-bold fst-italic">Welcome to courseella</small>
+          <h1 className="fw-bold mt-2">Learn without limits...</h1>
+          <p className="desc-color">
+            Start, switch, or advance your career with more than 5,200 courses,
+            Professional Certificates, and degrees from world-class universities
+            and companies.
+          </p>
+          <Button className="btn-color border-0 mt-3">
+            <Link to="/courses" className="text-white text-decoration-none">Get started now <FaArrowRight/> </Link>
+          </Button>
+          <Button className="ms-2 mt-3 btn-color border-0">
+            <Link to="/courses" className="text-white text-decoration-none">View Course <FaArrowRight/></Link>
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <Navbar bg="light" expand="lg">
+  return (
+    <Navbar bg="light" fixed="top" expand="lg">
       <Container>
-        <Navbar.Brand href="/home" className='fw-bold'>Courseella</Navbar.Brand>
+        <Navbar.Brand href="/home" className="fw-bold">
+          Courseella
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link className='text-decoration-none text-muted my-1 my-sm-0 mx-3 ' to="/home">Home</Link>
-            <Link className='text-decoration-none text-muted my-1 my-sm-0 mx-3 ' to="/courses">Courses</Link>
-            <Link className='text-decoration-none text-muted my-1 my-sm-0 mx-3 ' to="/faq">FAQ</Link>
-            <Link className='text-decoration-none text-muted my-1 my-sm-0 mx-3 ' to="/blog">Blog</Link>
+            <Link
+              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              to="/home"
+            >
+              Home
+            </Link>
+            <Link
+              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              to="/courses"
+            >
+              Courses
+            </Link>
+            <Link
+              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              to="/faq"
+            >
+              FAQ
+            </Link>
+            <Link
+              className="text-decoration-none text-dark my-1 my-sm-0 mx-3 "
+              to="/blog"
+            >
+              Blog
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 };
 
 export default Header;
